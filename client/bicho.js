6a5efe20be3d06bfc6c3eaace26c7b50329a30c1
BlocDeNotas <blabla12vitor@gmail.com>
2017-03-06 18:26:10 +0100

    Me dejé algo de código residual :c

--- client/bicho.js
+++ client/bicho.js
@@ -219,26 +219,3 @@ function gusanoFase3(bicho) {
     var pataDrc3_4 = new Nodo(bicho, TipoNodo.FLEXIBLE, pataDrc3_3, 0, 7);
     new Nodo(bicho, TipoNodo.ESTATICO, pataDrc3_4, 0, 7);
 }
-
-function drawEllipseByCenter(ctx, cx, cy, w, h) {
-  drawEllipse(ctx, cx - w/2.0, cy - h/2.0, w, h);
-}
-
-function drawEllipse(ctx, x, y, w, h) {
-  var kappa = .5522848,
-      ox = (w / 2) * kappa, // control point offset horizontal
-      oy = (h / 2) * kappa, // control point offset vertical
-      xe = x + w,           // x-end
-      ye = y + h,           // y-end
-      xm = x + w / 2,       // x-middle
-      ym = y + h / 2;       // y-middle
-
-  ctx.beginPath();
-  ctx.moveTo(x, ym);
-  ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
-  ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
-  ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
-  ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
-  //ctx.closePath(); // not used correctly, see comments (use to close off open path)
-  ctx.stroke();
-}
