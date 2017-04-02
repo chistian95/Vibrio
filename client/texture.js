4793fa752703920a011e01a318498693f1449431
BlocDeNotas <blabla12vitor@gmail.com>
2017-04-02 23:47:20 +0200

    Borrar imagenes por confrictos del merge.

--- client/texture.js
+++ client/texture.js
@@ -19,7 +19,7 @@ uiZise = PIXI.Texture.fromImage('assets/img/zise.png');
 uiNodos = PIXI.Texture.fromImage('assets/img/ganarNodos.png');
 uiCoraza = PIXI.Texture.fromImage('assets/img/coraza.png');
 barraExp =  PIXI.Texture.fromImage('assets/img/expBar.png');
-exp =  PIXI.Texture.fromImage('assets/img/2.png');
+exp =  PIXI.Texture.fromImage('assets/img/exp.png');
 
 /*PLANTAS
 =====================================================================*/
