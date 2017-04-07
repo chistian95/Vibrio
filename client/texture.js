55b790432b1c34c27d6d1cb26d830e09dcfb6b28
Dait <daitdoce@gmail.com>
2017-04-07 02:21:36 +0200

    Cambios transticiones y transformaciones nodoInicial

--- client/texture.js
+++ client/texture.js
@@ -12,11 +12,11 @@ parallax2 = PIXI.Texture.fromImage('assets/img/parallax2.png');
 
 /*UI
 =====================================================================*/
-uiPincho = PIXI.Texture.fromImage('assets/img/ico-pincho.png');
-uiOjo = PIXI.Texture.fromImage('assets/img/ico-ojo.png');
-uiTentaculo = PIXI.Texture.fromImage('assets/img/ico-tentaculo.png');
+uiPincho = PIXI.Texture.fromImage('assets/img/ico-pinchos.png');
+uiOjo = PIXI.Texture.fromImage('assets/img/ico-ojos.png');
+uiTentaculo = PIXI.Texture.fromImage('assets/img/ico-tentaculos.png');
 uiZise = PIXI.Texture.fromImage('assets/img/ico-size.png');
-uiNodos = PIXI.Texture.fromImage('assets/img/ico-nodo.png');
+uiNodos = PIXI.Texture.fromImage('assets/img/ico-nodos.png');
 uiCoraza = PIXI.Texture.fromImage('assets/img/ico-coraza.png');
 barraExp =  PIXI.Texture.fromImage('assets/img/expBar.png');
 exp =  PIXI.Texture.fromImage('assets/img/exp.png');
